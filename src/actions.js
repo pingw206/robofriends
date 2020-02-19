890ff6f55fe3d400c820fdd539cbb75ab83f2f70
wenping <pingw206@gmail.com>
2020-02-19 12:57:12 +0800

    redux middleware

--- src/actions.js
+++ src/actions.js
@@ -2,10 +2,8 @@ import {CHANGE_SEARCH_FIELD} from './constants.js';
 
 
 
-export const setSearchField = (text) => {
-  console.log(text);
-  return {
+export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD, //防止error不好定位，将字符串写成常量
     payload: text
-  };
-} 
+
+} )
